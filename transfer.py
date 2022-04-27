610f221cae39f16a6a862465dd22ce8ea79940ed
oliverstoll <oliverstoll.berlin@gmail.com>
2022-04-27 20:44:21 +0200

    small changes

--- transfer.py
+++ transfer.py
@@ -9,23 +9,23 @@ from  tensorflow.keras.applications.resnet50 import preprocess_input
 from keras import backend as K
 
 
-def recall_m(y_true, y_pred):
+def RC(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + K.epsilon())
     return recall
 
 
-def precision_m(y_true, y_pred):
+def PR(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
     precision = true_positives / (predicted_positives + K.epsilon())
     return precision
 
 
-def f1_m(y_true, y_pred):
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
+def F1(y_true, y_pred):
+    precision = PR(y_true, y_pred)
+    recall = RC(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 
@@ -46,8 +46,6 @@ BASE_LR = 0.002
 LR_REDUCTION = 0.08
 EPOCHS = 20
 
-
-
 # load datasets via image data generator, that rescales the images and preprocesses them with the function of resnet50
 generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, preprocessing_function=preprocess_input)
 train_dataset = generator.flow_from_directory(train_dir, batch_size=BATCH_SIZE, target_size=IMG_SIZE)
@@ -62,6 +60,7 @@ base_model = tf.keras.Sequential([base_model_layer])
 # create a data augmentation layer for improving data generalization
 data_augmentation = tf.keras.Sequential([
   tf.keras.layers.RandomFlip('horizontal'),
+  tf.keras.layers.RandomFlip('vertical'),
   tf.keras.layers.RandomRotation(0.2),
 ])
 
@@ -70,7 +69,8 @@ prediction_layer = tf.keras.layers.Dense(21)
 
 # create model
 inputs = tf.keras.Input(shape=IMG_SHAPE)
-x = data_augmentation(inputs)
+x = inputs
+x = data_augmentation(x)
 x = base_model(x, training=False)
 x = tf.keras.layers.Dropout(0.2)(x)
 outputs = prediction_layer(x)
@@ -78,9 +78,11 @@ model = tf.keras.Model(inputs, outputs)
 
 # compile the model
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=BASE_LR),
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy', f1_m, precision_m, recall_m])
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy', F1, PR, RC])
 
 # train the model
 callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
 history = model.fit(train_dataset, epochs=EPOCHS, validation_data=validation_dataset, callbacks=[callback])
 
+# save the model to output directory
+model.save("output/model.h5")
